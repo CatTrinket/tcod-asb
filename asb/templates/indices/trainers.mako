e597367177f98adce4dfc07822bf9979472c79cf
Lynn "Zhorken" Vaughan <zhorken@gmail.com>
2014-06-03 11:41:25 -0400

    Flesh out the big trainer list. #14

--- asb/templates/indices/trainers.mako
+++ asb/templates/indices/trainers.mako
@@ -1,8 +1,31 @@
 <%inherit file='/base.mako'/>\
+<%namespace name="h" file="/helpers/helpers.mako"/>\
 <%block name='title'>Trainers - The Cave of Dragonflies ASB</%block>\
 
-<ul>
-    % for t in trainers:
-    <li><a href="/trainers/${t.identifier}">${t.name}</a></li>
+<table>
+<thead>
+    <tr>
+        <th>Name</th>
+        <th>Money</th>
+        <th>Pkmn</th>
+        <th>Active Squad</th>
+    </tr>
+</thead>
+
+<tbody>
+    % for trainer, pokemon_count in trainers:
+    <tr>
+        <td class="focus-column">${h.link(trainer)}</td>
+        <td class="price">$${trainer.money}</td>
+        <td class="stat">${pokemon_count}</td>
+        <td>
+            % for pokemon in trainer.squad:
+<a href="${request.resource_url(pokemon.__parent__, pokemon.__name__)}">\
+${h.pokemon_form_icon(pokemon.form, pokemon.gender)}\
+</a>\
+            % endfor
+        </td>
+    </tr>
     % endfor
-</ul>
+</tbody>
+</table>
--- asb/views/trainer.py
+++ asb/views/trainer.py
@@ -1,5 +1,6 @@
 from pyramid.view import view_config
-from sqlalchemy.orm.exc import NoResultFound
+import sqlalchemy as sqla
+import sqlalchemy.orm
 
 from asb import db
 from asb.resources import TrainerIndex
@@ -8,9 +9,20 @@ from asb.resources import TrainerIndex
 def trainer_index(context, request):
     """The index of all the trainers in the league."""
 
+    pokemon_count = (
+        db.DBSession.query(db.Pokemon.trainer_id, sqla.func.count('*')
+            .label('count'))
+        .select_from(db.Pokemon)
+        .group_by(db.Pokemon.trainer_id)
+        .subquery()
+    )
+
     trainers = (
-        db.DBSession.query(db.Trainer)
-        .filter_by(unclaimed_from_hack=False)
+        db.DBSession.query(db.Trainer, pokemon_count.c.count)
+        .select_from(db.Trainer)
+        .join(pokemon_count, pokemon_count.c.trainer_id == db.Trainer.id)
+        .options(sqla.orm.subqueryload('squad'))
+        .filter(db.Trainer.unclaimed_from_hack == False)
         .order_by(db.Trainer.name)
         .all()
     )
