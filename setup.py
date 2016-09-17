29251cd658a4b56e4715fb68a4ec1d59724f33fe
Lynn "Zhorken" Vaughan <zhorken@gmail.com>
2016-09-16 22:43:29 -0400

    Run on Python 3.5.
    Hopefully that's all?  It definitely RUNS now, and I can't find any
    specific pages that aren't working.

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ import setuptools
 # I'm going to just give exact versions that I know will work
 requires = [
     'alembic==0.7.4',
-    'beautifulsoup4==4.3.2',
-    'bleach==1.4.1',
+    'beautifulsoup4==4.5.1',
+    'bleach==1.4.3',
     'Markdown==2.4.1',
     'pbkdf2==1.3',
     'pyramid==1.5',
