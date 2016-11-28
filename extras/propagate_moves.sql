4518027d7c99e1801202396af370546c20db86b8
Lynn "Zhorken" Vaughan <zhorken@gmail.com>
2016-11-28 00:24:37 -0500

    Update propagate_moves.sql for Alola Forms.
    This is pretty gross lmao.  It associates forms with the corresponding
    prevo form based on form_order, defaulting to the prevo's form 1.  This
    is technically wrong in a handful of places, but none that actually make
    a difference — e.g. it means that Mothim only inherits moves from Plant
    Burmy, but whatever.

--- extras/propagate_moves.sql
+++ extras/propagate_moves.sql
@@ -12,15 +12,28 @@ with recursive prevos (evo_id, prevo_id) as (
 
     select prevos.evo_id, ps.evolves_from_species_id from prevos
     join pokemon_species ps on prevos.prevo_id=ps.id
+),
+
+prevo_forms (evo_id, prevo_id) as (
+    select pf.id, pf_pre.id
+    from pokemon_forms pf
+    join prevos p on pf.species_id = p.evo_id
+    join pokemon_forms pf_pre on p.prevo_id = pf_pre.species_id
+    where pf.form_order = pf_pre.form_order or (
+        pf_pre.form_order = 1 and pf.form_order not in (
+            select form_order from pokemon_forms pf_pre_2
+            where p.prevo_id = pf_pre_2.species_id
+        )
+    )
 )
 
 insert into pokemon_form_moves
-select pfa.pokemon_form_id, pfa_pre.move_id
-from pokemon_form_moves pfa
-join pokemon_forms pf on pfa.pokemon_form_id=pf.id
-join prevos p on pf.species_id=p.evo_id
-join pokemon_forms pf_pre on p.prevo_id=pf_pre.species_id
-join pokemon_form_moves pfa_pre on pf_pre.id=pfa_pre.pokemon_form_id
+select pfm.pokemon_form_id, pfm_pre.move_id
+from pokemon_form_moves pfm
+join pokemon_forms pf on pfm.pokemon_form_id=pf.id
+join prevo_forms pre_f on pf.id = pre_f.evo_id
+join pokemon_forms pf_pre on pre_f.prevo_id = pf_pre.id
+join pokemon_form_moves pfm_pre on pf_pre.id=pfm_pre.pokemon_form_id
 except select * from pokemon_form_moves;
 
 -- Propagate moves between forms
@@ -32,7 +45,7 @@ join pokemon_forms pf2 on pf.species_id=pf2.species_id
 join pokemon_form_moves pfa2 on pf2.id=pfa2.pokemon_form_id
 where pf.species_id not in (
     select id from pokemon_species
-    where identifier in ('wormadam', 'rotom', 'kyurem', 'meowstic')
+    where identifier in ('rotom', 'kyurem') or not can_switch_forms
 )
 except select * from pokemon_form_moves;
 
