52b9b1f554cbb6bfb98a142455947addc2f86dbe
Trinket Holloway <trinket.git@catseyemarble.com>
2024-01-16 11:50:40 -0500

    Upate requirements.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requires = [
     'alembic==1.0.9',
     'bbcode==1.0.33',
     'beautifulsoup4==4.5.1',
-    'bleach==1.4.3',
+    'bleach==6.1.0',
     'Markdown==2.4.1',
     'pbkdf2==1.3',
     'pyramid==1.5',
@@ -17,10 +17,18 @@ requires = [
     'pyramid_tm==0.7',
     'SQLAlchemy==1.3.3',
     'transaction==2.4.0',
-    'waitress==0.8.7',
+    'waitress==2.1.2',
     'WTForms==2.2.1',
     'zope.sqlalchemy==1.1',
 ]
+
+extras_require = {
+    'production': [
+        'gunicorn==21.2.0',
+        'psycopg2==2.9.9',
+        'pycrypto==2.6.1',
+    ]
+}
 
 entry_points = {
     'paste.app_factory': 'main = asb:main',
@@ -34,5 +42,6 @@ setuptools.setup(
     zip_safe=False,
     test_suite='asb',
     install_requires=requires,
+    extras_require=extras_require,
     entry_points=entry_points
 )
